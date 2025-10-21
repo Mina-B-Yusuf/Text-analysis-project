dbfed63d7fe7bf476e75869ec197b1564b79db61
Mina Yusuf <min.you.tam26@gmail.com>
2025-10-21 13:23:58 +0200

    old functions and menu
    This script reads a text file and provides a menu for various text analysis options, including statistics and word frequency analysis.

---
+++ draft.py
@@ -0,0 +1,182 @@
+with open("sample.txt", "r") as f:
+    print(f.readline())
+    Once upon a time there was a flower.
+The flower was yellow and bright.
+It grew beside a calm river.
+
+import matplotlib.pyplot as plt
+
+while True: #keep looping forever — until I manually tell it to stop
+    print ('''--- Menu ---
+    1. Load text file
+    2. display basic statistics
+    3. word frequency analysis
+    4. sentence analysis
+    5. character analysis
+    6. export resultat
+    7. exit''')
+    choice = int(input('enter your choice'))
+    if choice == 1 :
+        print (1)   # load text file 
+    elif choice == 2:
+        print (2)   # display basic statistics
+    elif choice == 3:
+        print (3)   # word frequency analysis
+    elif choice == 4:
+        print (4)   # sentence analysis
+    elif choice == 5:
+        print (5)   # character analysis
+    elif choice == 6:
+        print (6)   # export resultat
+    else: 
+        break      #break the loop
+        print (7)   # exit
+    print('press enter to continue...')
+    
+def number_of_lines_f(text):
+    number_of_lines = 0
+    for line in text:
+        number_of_lines += 1
+    return number_of_lines
+
+def number_of_words_f(text):
+    number_of_words = 0
+    for line in text:
+        words = line.split()
+        for word in words:
+            number_of_words += 1
+    return number_of_words
+
+def number_of_characters_f(text):
+    number_of_character = 0
+    for line in text:
+        for char in line:
+            if char != ' ':
+                number_of_character += 1
+    return number_of_character
+
+def average_words_per_line_f(text):
+    number_of_lines = 0
+    number_of_words = 0
+    average_words_per_lines = []
+
+    for line in text:
+        number_of_lines += 1
+        words = line.split()
+        for word in words:
+            number_of_words += 1
+        average_words_per_lines.append(number_of_words)
+
+    average_words_per_line = (sum(average_words_per_lines))/(number_of_lines)
+    return average_words_per_line
+
+def average_character_per_word_f(text):
+    average_character_per_words = []
+    char = 0
+    number_of_words = 0
+    for line in text:
+        words = line.split()
+        for word in words:
+            number_of_words += 1
+            char = len(word)
+            average_character_per_words.append(char)
+    average_character_per_word = (sum(average_character_per_words))/(number_of_words)
+    return average_character_per_word
+
+def top_10_most_common_words(text):
+    list_of_words = []
+    for line in text:
+        words = line.split()
+        for word in words:
+            list_of_words.append(word.lower())
+    
+    frequencies = []
+    for word in list_of_words :
+        frequencies.append(list_of_words.count(word))
+    frequencies.sort(reverse=True)
+    
+    list_of_words_in_order = []
+    for word in list_of_words:
+        if list_of_words.count(word) == frequencies[0]:
+            frequencies.pop(0)
+            list_of_words_in_order.append(word)
+    return list_of_words_in_order[ : :10]
+
+
+            
+def distribution_of_words_lengths(text):
+    average_word_length_per_line = []
+    number_of_line = 0
+    for line in text:
+        words = line.split()
+        words = 0
+        number_of_line += 1
+
+        number_of_character = []
+        for word in words:
+            words += 1
+            number_of_characters = len(word)
+            number_of_character.append(number_of_characters)
+        average_word_length_per_line.append(sum(number_of_character)/(words))
+    
+
+            
+def distribution_of_words_lengths(text):
+    different_character_legnth = []
+
+    for line in text:
+        words = line.split()
+        words = 0
+
+        number_of_character = []
+        for word in words:
+            words += 1
+            number_of_characters = len(word)
+            number_of_character.append(number_of_characters)
+    
+    
+    for i in (1, 100):
+
+
+
+
+
+
+def unique_words(text):
+    list_of_words = []
+    for line in text:
+        words = line.split()
+        for word in words:
+            list_of_words.append(word.lower())
+    
+    frequencies = []
+    for word in list_of_words :
+        frequencies.append(list_of_words.count(word))
+    frequencies.sort(reverse=True)
+    
+    list_of_words_in_order = []
+    for word in list_of_words:
+        if list_of_words.count(word) == frequencies[0]:
+            frequencies.pop(0)
+            list_of_words_in_order.append(word)
+    return list_of_words_in_order[ : :-10]
+
+
+def words_that_appear_once_f(text):
+    list_of_words = []
+    for line in text:
+        words = line.split()
+        for word in words:
+            list_of_words.append(word.lower())
+            
+    words_that_appear_once = []
+    for word in list_of_words:
+        if list_of_words.count(word) == 1:
+            frequencies.pop(0)
+            words_that_appear_once.append(word)
+    return words_that_appear_once
+
+def letter_frequency(text):
+
+
+

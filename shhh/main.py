2550d3b03f91284f3c2af6018833c8a55ed57ce7
Mina Yusuf <min.you.tam26@gmail.com>
2025-10-27 12:31:23 +0000

    updated

--- shhh/main.py
+++ shhh/main.py
@@ -1,5 +1,5 @@
 from stats_functions import load_data, basic_statistics, word_analysis, sentence_analysis, character_analysis
-
+from visuals import basic_statistics_visuals, word_analysis_visuals, sentence_analysis_visuals, character_analysis_visuals
 data = load_data()
 
 while True: #keep looping forever — until I manually tell it to stop
@@ -22,22 +22,52 @@ while True: #keep looping forever — until I manually tell it to stop
               1. Visuals for Basics Statistics
               2. Back to menu''')
         choice = int(input('Enter your choice: '))
-        if choice == 1
+        if choice == 1:
+            basic_statistics_visuals(data)
+
+        else:
+            break 
+
+
 
     elif choice == 3: # word frequency analysis
         print ("--------- Word Analysis ---------")
         word_analysis(data)
-        print()
+        print('''
+              1. Visuals for Basics Statistics
+              2. Back to menu''')
+        choice = int(input('Enter your choice: '))
+        if choice == 1:
+            word_analysis_visuals(data)
+            
+        else:
+            break 
 
     elif choice == 4:
         print ("--------- Sentence Analysis ---------")
         sentence_analysis(data)
         print()
+        print('''
+              1. Visuals for Basics Statistics
+              2. Back to menu''')
+        choice = int(input('Enter your choice: '))
+        if choice == 1:
+            sentence_analysis_visuals(data)
+        else:
+            break 
 
     elif choice == 5:
         print ("--------- Character Analysis ---------")
         character_analysis(data)
         print()
+        print('''
+              1. Visuals for Basics Statistics
+              2. Back to menu''')
+        choice = int(input('Enter your choice: '))
+        if choice == 1:
+            character_analysis_visuals(data)
+        else:
+            break 
 
     elif choice == 6:
         print (6)   # export resultat
